e211136d625f0eceaad26e070d29debe8a762a31
Win Htet Aung <whaung1998@gmail.com>
2021-01-08 20:01:41 +0630

    code fix

--- ProjectAPSrt/asgi.py
+++ ProjectAPSrt/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter  # for channels
 from django.core.asgi import get_asgi_application
+import DataManagement.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectAPSrt.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_application(),
+        'websocket': AuthMiddlewareStack(
+            URLRouter(
+                DataManagement.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
--- ProjectAPSrt/settings.py
+++ ProjectAPSrt/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +25,13 @@ SECRET_KEY = '$-k*9ho2^h9o$hlvxf)_2hjh2ei(vi0nev&nz=nc$w8fir7ep('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'RealtimeAPS',
     'DataManagement',
-    'channels',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ProjectAPSrt.wsgi.application"
+WSGI_APPLICATION = 'ProjectAPSrt.wsgi.application'
 ASGI_APPLICATION = "ProjectAPSrt.asgi.application"
 CHANNEL_LAYERS = {
     'default': {
@@ -86,67 +85,15 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'HOST': 'localhost',
-#        'USER': 'root',
-#        'PASSWORD': 'MySQLR00tpwd',
-#        'NAME': 'projectaps'
-#    }
-#}
-
-# Install PyMySQL as mysqlclient/MySQLdb to use Django's mysqlclient adapter
-# See https://docs.djangoproject.com/en/2.1/ref/databases/#mysql-db-api-drivers
-# for more information
-import pymysql  # noqa: 402
-pymysql.version_info = (1, 4, 6, 'final', 0)  # change mysqlclient version
-pymysql.install_as_MySQLdb()
-
-# [START db_setup]
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
-###
-#            'ENGINE': 'django.db.backends.mysql',
-#            'HOST': '/cloudsql/<your-cloudsql-connection-string>',
-#            'NAME': 'polls',
-#            'USER': '<your-database-user>',
-#            'PASSWORD': '<your-database-password>',
-###
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/projectaps-296207:us-central1:projectaps',
-               #testdjangokzw:asia-southeast1:polls-instance',
-            'USER': 'projectaps',
-            'PASSWORD': 'projectaps',
-            'NAME': 'projectaps',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'MySQLR00tpwd',
+        'NAME': 'iotdb',
     }
-else:
-    # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy via command line:
-    #
-    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    #
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-#            'ENGINE': 'django.db.backends.mysql',
-#            'HOST': '127.0.0.1',
-#            'PORT': '3306',
-#            'NAME': 'polls',
-#            'USER': '<your-database-user>',
-#            'PASSWORD': '<your-database-password>',
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': 'localhost',
-            'PORT': '3306',
-            'NAME': 'projectaps',
-            'USER': 'projectaps',
-            'PASSWORD': 'projectaps',
-        }
-    }
+}
 
 
 # Password validation
@@ -184,9 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = 'static'   #for GAE
+
 STATIC_URL = '/static/'
 
-# login
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
